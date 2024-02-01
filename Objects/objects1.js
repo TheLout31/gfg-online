70d9613545326b70ee98e0a1f3015d7c9901be11
Imran <imranjaleel2331@gmail.com>
2024-02-01 22:18:01 +0530

    questions practice

--- Objects/objects1.js
+++ Objects/objects1.js
@@ -1,19 +1,71 @@
-let arr = [1, 2, 3, 4, 5];
+// let arr = [1, 2, 3, 4, 5];
+// let b = `Hello`;
+// // function hello (){
+// //   var a =10;
+// // }
 
-function cal(val, v) {
-  return val ** v;
-}
+// console.log(typeof b);
 
-function square(num, i, wrapper) {
-  let newarr = [];
-  for (let n of num) {
-    newarr.push(wrapper(n, i));
+//recursve
+// let sum = 0;
+// function total(n) {
+//   if (n === 1) {
+//     return n;
+//   }
+//   return n + total(n - 1);
+// }
+
+// let result = total(5);
+
+// console.log(result);
+
+// var array = [36, 25, 6, 15];
+
+// let a =array.reduce(function (accumulator, currentValue) {
+//   return accumulator + currentValue;
+// }, {}); // 36 + 25 + 6 + 15 = 82
+
+// console.log(a);
+
+// function noVowels(str) {
+//   let v = ["a", "e", "i", "o", "u"];
+//   let result = "";
+//   for (let char of str) {
+//     if (!v.includes(char)) {
+//       result += char;
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(noVowels("Hello how are you"));
+
+// function hashtag(str) {
+//   let temp = "";
+//   for (let i = 0; i < str.length; i++) {
+//     if (i < str.length - 4) {
+//       temp += str[i];
+//     } else {
+//       temp += "#";
+//     }
+//   }
+//   return temp;
+// }
+// // BIRTHDAY
+// console.log(hashtag("birthday"));
+
+function encode(str, n) {
+  let encoded = "";
+  let currposition = 0;
+  let newposition = 0;
+  let newStr = "";
+  for (let i in str) {
+    currposition = str.charCodeAt(i);
+    newposition = currposition + n;
+    newStr = String.fromCharCode(newposition);
+    encoded += newStr;
   }
-  return newarr;
+  return encoded;
 }
 
-let s = square(arr, 3, cal);
-
-
-console.log(s);
-
+console.log(encode("Imran", 5));
