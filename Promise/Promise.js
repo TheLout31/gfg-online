bf9d08fe39426db99a60a2afb56782aa85362a00
Imran <imranjaleel2331@gmail.com>
2024-02-05 21:10:46 +0530

    promise apply

---
+++ Promise/Promise.js
@@ -0,0 +1,40 @@
+const url = "https://reqres.in/api/products/3";
+
+// const promise = fetch(url)
+//   .then(function (res) {
+//     return res.json();
+//   })
+//   .then(function (data) {
+//     return data.data;
+//   })
+//   .then(function (newData) {
+//     console.log(newData.id);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// console.log(promise);
+
+function db() {
+  return 10 < 2 ? true : false;
+}
+
+console.log(db());
+
+let newPromise = new Promise((resolve, reject) => {
+  if (db === true) {
+    resolve("my code works");
+  } else {
+    const error = new Error("something went wrong");
+    reject("you code is wrong", error.message);
+  }
+});
+
+newPromise
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.log(error, "Your code failed");
+  });
