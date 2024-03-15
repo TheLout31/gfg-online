5f6f34aca842e16ca0a190e33305f2ac657afafb
Imran Jaleel <imranjaleel2331@gmail.com>
2024-03-15 23:39:12 +0530

    more on binary search

---
+++ BinarySearch/SearchInRotated.js
@@ -0,0 +1,26 @@
+const binarySearch = function (nums, target) {
+    let start = 0, end = nums.length - 1;
+    let mid = Math.floor((start + end) / 2);
+    while (start <= end) {
+        mid = Math.floor((start + end) / 2);
+        if (target === nums[mid]) {
+            return mid;
+        }
+        if (nums[start] <= nums[mid]) {
+            if (nums[start] <= target && nums[mid] >= target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        } else {
+            if (nums[end] >= target && nums[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+    }
+    return -1;
+};
+
+console.log(binarySearch([4, 5, 6, 1, 2, 3], 5));
--- LeetcodeArrayStrings/RotateArray.js
+++ LeetcodeArrayStrings/RotateArray.js
@@ -18,4 +18,4 @@ var rotate = function (nums, k) {
   }
 };
 
-console.log(rotate([-1, -100, 3, 99], 2));
+console.log(rotate([1, 2, 3, 99], 2));
